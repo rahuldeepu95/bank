ec9560491d0e00f53886dca79e246a5999900aac
unknown <rahuldeepu95@gmail.com>
2023-02-08 01:52:11 +0530

    validation

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { DataService } from '../services/data.service';
 
@@ -11,21 +12,27 @@ export class RegisterComponent {
 
   data = "Your banking partner."
 
+    
 
-  uname=''
-  acno=''
-  psw=''
-
-  constructor(private ds:DataService,private router:Router) { }
+  constructor(private ds:DataService,private router:Router,private fb:FormBuilder) { }
+// create a reactive form of register form
+registerForm=this.fb.group({
+  acno:['',[Validators.required,Validators.pattern('[0-9]+')]],
+  uname:['',[Validators.required,Validators.pattern('[a-zA-Z]+')]],
+  psw:['',[Validators.required,Validators.pattern('[0-9a-zA-Z]+')]]
+})
 
   ngOnInit(): void {
 
   }
 
   register() {
-    var uname=this.uname
-    var acno=this.acno
-    var psw=this.psw
+    var uname=this.registerForm.value.uname
+    var acno=this.registerForm.value.acno
+    var psw=this.registerForm.value.psw
+    if(this.registerForm.valid){
+
+    
 
     const result=this.ds.register(uname,acno,psw)
     if(result){
@@ -36,7 +43,10 @@ export class RegisterComponent {
       alert("User already exists")
     }
   }
-  
+  else{
+    alert('Invalid characters used')
+  }
+}
 
 }
 
